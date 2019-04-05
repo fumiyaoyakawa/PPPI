517cff3919edd335ac7244f6158cb54ff7e6313d
fumiya <fumiya.om@gmail.com>
2019-04-05 18:28:12 -0400

    commit mvc

---
+++ WebApi/WebApi/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApi.Models.Product> Products { get; set; }
+    }
+}
